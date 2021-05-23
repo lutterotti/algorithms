fcc3bc8dd853f543d757d194b1370d2de06530e5
Stef Lutterotti <luteroti@gmail.com>
2021-05-23 13:46:23 -0600

    small clean

--- src/utils/util-functions.ts
+++ src/utils/util-functions.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-expressions */
-import { cloneDeep } from "lodash";
 import { Item } from "../store/algorithmSlice";
 
 export function generateRandomNumberInterval(min: number, max: number ) {
@@ -20,31 +18,31 @@ export function mergeSort(queue: Item[]): Item[] {
   return queue;
 }
 
-const pivot = (arr: number[], start: number) => {
+const pivot = (queue: number[], start: number) => {
   const swap = (list: number[], a: number, b: number) => [list[a], list[b]] = [list[b], list[a]];
 
-  let pivot = arr[start],
+  let pivot = queue[start],
       pointer = start;
 
-  for (let i = start; i < arr.length; i++) {
-    if (arr[i] < pivot) {
+  for (let i = start; i < queue.length; i++) {
+    if (queue[i] < pivot) {
       pointer++;
-      swap(arr, pointer, i);
+      swap(queue, pointer, i);
     }
   };
-  swap(arr, start, pointer);
+  swap(queue, start, pointer);
 
   return pointer;
 }
 
-export const quickSortB = (arr, start: number, end: number) => {
-  let pivotIndex = pivot(arr, start);
+export const quickSortB = (queue: number[], start: number, end: number) => {
+  let pivotIndex = pivot(queue, start);
 
-  if (start >= end) return arr;
-  quickSortB(arr, start, pivotIndex);
-  quickSortB(arr, pivotIndex + 1, end);
+  if (start >= end) return queue;
+  quickSortB(queue, start, pivotIndex);
+  quickSortB(queue, pivotIndex + 1, end);
 
-  return arr;
+  return queue;
 };
 
 
