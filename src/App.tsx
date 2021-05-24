dec61b8349117eaf862bf130d51cb8446cdfd51a
Stef Lutterotti <luteroti@gmail.com>
2021-05-24 13:37:13 -0600

    feat: set up basis to change queue options

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../src/styles/mainStyles.scss';
 import DisplayQueue from './components/DisplayQueue';
+import { GenerateQueueOptions } from './components/GenerateQueueOptions';
 import SelectAlgorithm from './components/SelectAlgorithm';
 
 function App() {
   return (
     <div className="app">
       <SelectAlgorithm></SelectAlgorithm>
+      <GenerateQueueOptions></GenerateQueueOptions>
       <DisplayQueue></DisplayQueue>
     </div>
   );
--- src/store/algorithmSlice.ts
+++ src/store/algorithmSlice.ts
@@ -21,12 +21,24 @@ export interface Item {
   age: number;
 }
 
+export interface QueueOptions {
+  length_of_queue: number;
+  min_value: number;
+  max_value: number;
+}
+
 export interface AlgorithmState {
   queue: number[];
+  options: QueueOptions;
 }
 
 const initialState: AlgorithmState = {
-  queue: []
+  queue: [],
+  options: {
+    length_of_queue: 20,
+    min_value: 0,
+    max_value: 120
+  }
 };
 
 export const AlgorithmSlice = createSlice({
@@ -34,7 +46,8 @@ export const AlgorithmSlice = createSlice({
   initialState,
   reducers: {
     generateSortQueue: (state, _action: PayloadAction<null> ) => {
-      const new_queue: number[] = generateQueue(10, 0, 50);
+      const { length_of_queue, min_value, max_value } = state.options;
+      const new_queue: number[] = generateQueue(length_of_queue, min_value, max_value);
       return {...state, queue: new_queue}
     },
     quickSortQueue: (state: AlgorithmState, action: PayloadAction<number[]>) => {
@@ -47,12 +60,25 @@ export const AlgorithmSlice = createSlice({
       const sorted_queue = mergeSort(cloned_queue);
 
       return {...state, queue: sorted_queue};
+    },
+    updateQueueOptions: (state: AlgorithmState, action: PayloadAction<QueueOptions>) => {
+      const { length_of_queue, min_value, max_value } = action.payload;
+      const default_length_of_queue = state.options.length_of_queue;
+      const default_min_value = state.options.min_value;
+      const default_max_value = state.options.max_value;
+
+      return {...state, options: {
+        length_of_queue: length_of_queue ? length_of_queue : default_length_of_queue,
+        min_value: min_value ? min_value : default_min_value,
+        max_value: max_value ? max_value : default_max_value
+      }}
     }
   },
 });
 
-export const { generateSortQueue, quickSortQueue, mergeSortQueue } = AlgorithmSlice.actions;
+export const { generateSortQueue, quickSortQueue, mergeSortQueue, updateQueueOptions } = AlgorithmSlice.actions;
 
 export const getQueue = (state: RootState) => state.queue.queue;
+export const getQueueOptions = (state: RootState) => state.queue.options;
 
 export default AlgorithmSlice.reducer;
